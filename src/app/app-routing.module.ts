ba2481273bffd9ad878635a0c30f063e9870577a
dyanez11 <diegoayanezvega@hotmail.com>
2020-11-13 21:46:03 -0600

    fix: remove unnecesary components and fix description styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BooksComponent } from './components/books/books.component';
 import { EditComponent } from './components/edit/edit.component';
 import { LoginComponent } from './components/login/login.component';
-import { MostLikedComponent } from './components/most-liked/most-liked.component';
 import { ProductsComponent } from './components/products/products.component';
-import { PromotionsComponent } from './components/promotions/promotions.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 const routes: Routes = [
@@ -21,18 +18,6 @@ const routes: Routes = [
   {
     path: 'edit',
     component: EditComponent,
-  },
-  {
-    path: 'promotions',
-    component: PromotionsComponent,
-  },
-  {
-    path: 'liked',
-    component: MostLikedComponent,
-  },
-  {
-    path: 'books',
-    component: BooksComponent,
   },
 ];
 
